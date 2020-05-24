4e5737f83d11ea3e4da554a05954e919890d47be
danielbarretto <daniel_barretto98@hotmail.com>
2020-05-24 13:35:25 -0300

    Add ping and connect commands

--- client.cpp
+++ client.cpp
@@ -1,27 +1,29 @@
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <string>
+
 #include <iostream>
-#include <signal.h>
+#include <string>
 
 #include "irc.h"
 
 #define STOP_FLAG 0x0001
 #define EMPTY_FLAG 0x0000
+#define PING_FLAG 2
 
 int flag;
-//function that receives a messagem from server
+// function that receives a messagem from server
 void *recvmg(void *my_sock) {
   int sock = *((int *)my_sock);
   int len;
   char msg[BUFFER_SIZE];
 
-  //client thread always ready to receive message
+  // client thread always ready to receive message
   while ((len = recv(sock, msg, BUFFER_SIZE, 0)) > 0) {
     msg[len] = '\0';
     fputs(msg, stdout);
@@ -40,7 +42,7 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in ServerIp;
   std::string client_name;
 
-  //Signal handling
+  // Signal handling
   /*struct sigaction act;
   memset(&act, 0, sizeof(struct sigaction));
   act.sa_sigaction = interrupt_handler;
@@ -48,42 +50,58 @@ int main(int argc, char *argv[]) {
   sigaction(SIGINT, &act, NULL);*/
   signal(SIGINT, SIG_IGN);
 
-  if(argc < 3){
+  /*if (argc < 3) {
     std::cout << "Modo de uso: " << argv[0] << " nome PORT" << std::endl;
+  }*/
+
+  std::string command;
+  std::cout << "Bem vindo ao IRC!\nPara iniciar digite o comando /connect seguido de seu usuário!" << std::endl;
+  std::cin >> command >> client_name;
+  while(command != "/connect" && client_name==""){
+    std::cout << "Erro!\nDigite /connect seguido de seu usuário para iniciar!" <<std::endl;
+    std::cin >> command >> client_name;
   }
 
-  client_name = argv[1];
-  //socket configuration
-  sock = socket(AF_INET, SOCK_STREAM, 0);
+  std::string connectStr = "/connect ";
+  std::string::size_type i = client_name.find(connectStr);
+  if(i != std::string::npos)
+    client_name.erase(i, connectStr.length());
+
+  std::cout << "Bem vindo(a) " << client_name << std::endl;
   
-  ServerIp.sin_port = htons(atoi(argv[2]));
+  // socket configuration
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  //ServerIp.sin_port = htons(atoi(argv[2]));
+  ServerIp.sin_port = htons(2000);
   ServerIp.sin_family = AF_INET;
   ServerIp.sin_addr.s_addr = inet_addr("127.0.0.1");
-  
-  if ((connect(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp))) == -1){
+
+  if ((connect(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp))) == -1) {
     IRC::error("connect");
   }
   flag = EMPTY_FLAG;
   // creating a client thread which is always waiting for a message
   pthread_create(&recvt, NULL, recvmg, &sock);
- 
+
   // ready to read a message from console
   char msg[BUFFER_SIZE];
   std::string send_msg;
-  while (fgets(msg,BUFFER_SIZE - client_name.size(), stdin) > 0) {
-    //Format and send message to server
-    send_msg = '\n'+ client_name + ": " + msg;
+  while (fgets(msg, BUFFER_SIZE - client_name.size(), stdin) > 0) {
+    // Format and send message to server
+    send_msg = '\n' + client_name + ": " + msg;
     len = write(sock, send_msg.c_str(), send_msg.length());
-    if (len < 0){
+    if (len < 0) {
       std::cout << "\nWarning: Message not sent!\n";
     }
-    //Compare with commands and execute if true
-    if(!strncmp(msg, "/quit\n", 6)){
+    // Compare with commands and execute if true
+    if (!strncmp(msg, "/quit\n", 6)) {
       flag = STOP_FLAG;
     }
-    //Test flag
-    if(flag == STOP_FLAG){
-      std::cout << "Thank you for using our IRC, hope you enjoyed your experience!\n";
+    // Test flag
+    if (flag == STOP_FLAG) {
+      std::cout
+          << "Thank you for using our IRC, hope you enjoyed your experience!\n";
       break;
     }
   }
--- server.cpp
+++ server.cpp
@@ -1,51 +1,54 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
-#include <vector>
-#include <unordered_map>
+
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 #include "irc.h"
 
 pthread_mutex_t mutex;
 
-typedef struct client{
+typedef struct client {
   int sockfd;
+  std::string nick;
   pthread_t tid;
 } Client;
 
-struct tinfo{
+struct tinfo {
   int fd;
   char* msg;
 };
 
 std::vector<Client*> clients;
-//Hashmap for finding clients via its socket file descriptor
+// Hashmap for finding clients via its socket file descriptor
 // Key: client->sockfd | Value: client
-std::unordered_map<int, Client*> clients_hash = std::unordered_map<int, Client*>();
+std::unordered_map<int, Client*> clients_hash =
+    std::unordered_map<int, Client*>();
 
 void add_client(Client* new_client);
 void remove_client(int sockfd);
 void* send_client_msg(void* sockfd_msg);
-void* recvmg(void *client_sock);
+void* recvmg(void* client_sock);
 
-//function that sends a message received by server to all clients
-void sendtoall(char *msg, int curr) {
+// function that sends a message received by server to all clients
+void sendtoall(char* msg, int curr) {
   pthread_mutex_lock(&mutex);
 
   pthread_t send_msgt;
 
-  //Iterate through all clients
+  // Iterate through all clients
   for (auto it = clients.begin(); it != clients.end(); it++) {
-    if(DEBUG_MODE) std::cout << (*it)->sockfd << std::endl; 
+    if (DEBUG_MODE) std::cout << (*it)->sockfd << std::endl;
     int fd = (*it)->sockfd;
-    //Send message to client if its not the one who wrote it
+    // Send message to client if its not the one who wrote it
     if (fd != curr) {
       struct tinfo* info = new struct tinfo;
       info->fd = fd;
@@ -56,164 +59,159 @@ void sendtoall(char *msg, int curr) {
   pthread_mutex_unlock(&mutex);
 }
 
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   struct sockaddr_in ServerIp;
   pthread_t recvt;
 
   int sock = 0, client_fd = 0;
 
-  //socket configuration
+  // socket configuration
   ServerIp.sin_family = AF_INET;
   ServerIp.sin_port = htons(atoi(argv[1]));
   ServerIp.sin_addr.s_addr = inet_addr("127.0.0.1");
-  
+
   sock = socket(AF_INET, SOCK_STREAM, 0);
 
-  if (bind(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp)) == -1){
+  if (bind(sock, (struct sockaddr*)&ServerIp, sizeof(ServerIp)) == -1) {
     IRC::error("bind");
-  } 
-  else
+  } else
     std::cout << "Server Started" << std::endl;
 
-  //set socket in a passive mode that waits a client connection
-  if (listen(sock, 20) == -1){
+  // set socket in a passive mode that waits a client connection
+  if (listen(sock, 20) == -1) {
     IRC::error("listen");
   }
 
-  //Accept new client and create thread for receiving messages
+  // Accept new client and create thread for receiving messages
   while (1) {
-    if ((client_fd = accept(sock, (struct sockaddr *)NULL, NULL)) < 0){
+    if ((client_fd = accept(sock, (struct sockaddr*)NULL, NULL)) < 0) {
       perror("accept");
     }
-    //Create client struct in memory
-    Client* new_client = (Client*) malloc(sizeof(Client));
+    // Create client struct in memory
+    Client* new_client = (Client*)malloc(sizeof(Client));
     new_client->sockfd = client_fd;
-    
-    //Add to hashmap and vector
+
+    // Add to hashmap and vector
     add_client(new_client);
 
-    //Create thread for receiving client's messages
+    // Create thread for receiving client's messages
     pthread_create(&recvt, NULL, &recvmg, (void*)new_client);
-    //Assign thread_id to client
+    // Assign thread_id to client
     new_client->tid = recvt;
 
-    //Send connection message to all clients on server
+    // Send connection message to all clients on server
     char connection_message[] = "\nServer: A client has joined\n";
     sendtoall(connection_message, client_fd);
   }
   return 0;
 }
 
-//function that adds client to list
-void add_client(Client* new_client){
-    pthread_mutex_lock(&mutex);
-    clients.push_back(new_client);
-    clients_hash[new_client->sockfd] = new_client;
-    pthread_mutex_unlock(&mutex);
+// function that adds client to list
+void add_client(Client* new_client) {
+  pthread_mutex_lock(&mutex);
+  clients.push_back(new_client);
+  clients_hash[new_client->sockfd] = new_client;
+  pthread_mutex_unlock(&mutex);
 }
 
-//funciton that removes client from list
-void remove_client(int sockfd){
-    pthread_mutex_lock(&mutex);
-    //Find client to be removed, swap with the end, remove from the end.
-    for(int i = 0; i < clients.size(); i++){
-      if(clients[i]->sockfd == sockfd){
-        Client* c = clients[i];
-        clients[i] = clients.back();
-        clients.pop_back();
-        //close client's file descriptor
-        close(sockfd);
-        break;
-      }
+// funciton that removes client from list
+void remove_client(int sockfd) {
+  pthread_mutex_lock(&mutex);
+  // Find client to be removed, swap with the end, remove from the end.
+  for (int i = 0; i < clients.size(); i++) {
+    if (clients[i]->sockfd == sockfd) {
+      Client* c = clients[i];
+      clients[i] = clients.back();
+      clients.pop_back();
+      // close client's file descriptor
+      close(sockfd);
+      break;
     }
-    pthread_mutex_unlock(&mutex);
+  }
+  pthread_mutex_unlock(&mutex);
 }
 
-//function that receives a message from client
-void *recvmg(void *client_sock) {
+// function that receives a message from client
+void* recvmg(void* client_sock) {
   int sock = ((Client*)client_sock)->sockfd;
   char msg[BUFFER_SIZE] = {0};
   int len;
 
-  //Receive message
+  // Receive message
   while ((len = recv(sock, msg, BUFFER_SIZE, 0)) > 0) {
     msg[len] = '\0';
-    //Handle /quit command
+    // Handle /quit command
     std::string command = msg;
-    if(command.find("/quit") == std::string::npos){
+    if (command.find("/quit\n") == std::string::npos &&
+        command.find("/ping\n") == std::string::npos) {
       sendtoall(msg, sock);
-    }
-    else{
-      //Send quit message to all and break while
-      if(DEBUG_MODE) sprintf(msg, "Server: %d has quit\n", sock);
-      else sprintf(msg, "Server: A client has quit\n");
+    } else if (command.find("/quit\n") != std::string::npos) {
+      // Send quit message to all and break while
+      if (DEBUG_MODE)
+        sprintf(msg, "Server: %d has quit\n", sock);
+      else
+        sprintf(msg, "Server: A client has quit\n");
       sendtoall(msg, sock);
       break;
+    } else if (command.find("/ping\n") != std::string::npos) {
+      // Handle ping message
+      struct tinfo* pongMsg = new struct tinfo;
+      pongMsg->fd = sock;
+      char pong[] = "Server: pong\n";
+      pongMsg->msg = pong;
+      // Sends message only to client who sent ping
+      send_client_msg((void*)pongMsg);
     }
   }
   printf("Server: %d has quit\n", sock);
-  //Remove Client, close connection and end thread
+  // Remove Client, close connection and end thread
   remove_client(sock);
   return NULL;
 }
 
-void* send_client_msg(void* sockfd_msg)
-{
-  //Get params
-  int fd = ((struct tinfo*) sockfd_msg)->fd;
-  char* msg = ((struct tinfo*) sockfd_msg)->msg;
+void* send_client_msg(void* sockfd_msg) {
+  // Get params
+  int fd = ((struct tinfo*)sockfd_msg)->fd;
+  char* msg = ((struct tinfo*)sockfd_msg)->msg;
 
   // ? apagar
-  if(DEBUG_MODE) std::cout << "send_client_msg: sending to " << fd << msg << std::endl;
-  //Get client's thread id
+  if (DEBUG_MODE)
+    std::cout << "send_client_msg: sending to " << fd << " " << msg
+              << std::endl;
+  // Get client's thread id
   // ? e' operacao de leitura, entao nao e' pra da deadlock, neh ?
   pthread_mutex_lock(&mutex);
   pthread_t tid;
   auto c = clients_hash.find(fd);
-  if(c == clients_hash.end()){
+  if (c == clients_hash.end()) {
     // ? apagar
-    if(DEBUG_MODE) std::cout << "send_client_msg: no such client with fd " << fd << std::endl;
-  }
-  else{
+    if (DEBUG_MODE)
+      std::cout << "send_client_msg: no such client with fd " << fd
+                << std::endl;
+  } else {
     tid = c->second->tid;
   }
   pthread_mutex_unlock(&mutex);
 
   int num_fails = 0;
-  
-  //Send message and wait for confirmation
-  while(num_fails < 5)
-  {
-    //Sent sucessfully
-    if (send(fd, msg, strlen(msg), 0) > 0) 
-    {
-      break;
-    }
-    //Increament failure
-    else
-    {
-      num_fails++;
-      // ? apagar
-      if(DEBUG_MODE) std::cout << "send_client_msg: failed" << num_fails << std::endl; 
-    }
+
+  // Send message and wait for confirmation
+  while (num_fails < 5 && send(fd, msg, strlen(msg), 0) < 0) {
+    num_fails++;
+    // ? apagar
+    if (DEBUG_MODE)
+      std::cout << "send_client_msg: failed" << num_fails << std::endl;
   }
 
-  //Disconnect client if more than 5 failed tries
-  if(num_fails >= 5)
-  {
-    if(!pthread_cancel(tid))
-    {
+  // Disconnect client if more than 5 failed tries
+  if (num_fails >= 5) {
+    if (!pthread_cancel(tid)) {
       remove_client(fd);
-    }
-    else
-    {
+    } else {
       perror("send_client_msg: no thread with this id found");
     }
   }
-  
 
-  //End thread
+  // End thread
   return NULL;
 }
